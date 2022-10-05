c1447f499c80ff827f545406107989390c1c3060
USUARIO22 <USUARIO22@dell5556>
2022-10-05 14:56:12 -0500

    Mejoradas las pruebas

--- src/main/java/espoch/edu/ec/raicespolinomio/RaicesPolinomio.java
+++ src/main/java/espoch/edu/ec/raicespolinomio/RaicesPolinomio.java
@@ -1,21 +1,22 @@
 package espoch.edu.ec.raicespolinomio;
 import java.util.Scanner;
+import java.io.InputStream;
 
 public class RaicesPolinomio {
 
-    static double a;
-    static double b;
-    static double c;
+    static float a;
+    static float b;
+    static float c;
     
-    public static void Ingreso()throws Exception{
-        Scanner leer = new Scanner (System.in);
+    public static void Ingreso(InputStream in)throws Exception{
+        Scanner leer = new Scanner(in);
         try{
             System.out.print("Ingrese el valor de a: ");
-            a = leer.nextDouble();
+            a = leer.nextFloat();
             System.out.print("Ingrese el valor de b: ");
-            b = leer.nextDouble();
+            b = leer.nextFloat();
             System.out.print("Ingrese el valor de c: ");
-            c = leer.nextDouble();
+            c = leer.nextFloat();
         }catch(Exception e) {
             throw new Exception("Ingrese solo numeros");
         }  
@@ -23,9 +24,10 @@ public class RaicesPolinomio {
     
     public static void main(String[] args) {
         try {
-            Ingreso();
+            Ingreso(System.in);
             Polinomio miPolinomio = new Polinomio(a, b, c);
-            miPolinomio.CalcularRaiz();
+            System.out.println("x1 = " + miPolinomio.x1);
+            System.out.println("x2 = " + miPolinomio.x2);
         } catch (Exception e) {
             System.out.println(e);
         }
